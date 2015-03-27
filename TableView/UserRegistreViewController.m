3dd9b6be1401bd939fd8f15669003e7a3e73b322
Davi Rodrigues <davirdgs@MacBook-Pro-de-Davi.local>
2015-03-27 15:44:46 -0300

    Fiz as classes que faltavam

---
+++ TableView/UserRegistreViewController.m
@@ -0,0 +1,37 @@
+//
+//  UserRegistreViewController.m
+//  TableView
+//
+//  Created by Davi Rodrigues on 27/03/15.
+//  Copyright (c) 2015 Jessica Oliveira. All rights reserved.
+//
+
+#import "UserRegistreViewController.h"
+
+@interface UserRegistreViewController ()
+
+@end
+
+@implementation UserRegistreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
