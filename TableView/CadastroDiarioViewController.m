61b29cc94e36ac3e2b91318468864f0e49e7e28b
Davi Rodrigues <davirdgs@MacBook-Pro-de-Davi.local>
2015-03-27 15:32:17 -0300

    classes renomeadas

---
+++ TableView/CadastroDiarioViewController.m
@@ -0,0 +1,127 @@
+//
+//  TableViewController.m
+//  TableView
+//
+//  Created by Jessica Oliveira on 26/03/15.
+//  Copyright (c) 2015 Jessica Oliveira. All rights reserved.
+//
+
+#import "CadastroDiarioViewController.h"
+#import "ViewController.h"
+
+@interface CadastroDiarioViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *GlucoData;
+@property NSMutableArray *objects;
+@property NSInteger cell;
+@end
+
+@implementation CadastroDiarioViewController
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.cell = 1;
+//     Uncomment the following line to preserve selection between presentations.
+//     self.clearsSelectionOnViewWillAppear = NO;
+//    
+//     Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+//     self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    // Return the number of sections.
+    return 2;
+}
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+   
+    if(section == 0)
+        self.cell = 1;
+    else
+        self.cell+=3;
+    
+    return self.cell;
+}
+
+//- (IBAction)moreSymptom:(id)sender {
+//    if (!self.objects) {
+//        self.objects = [[NSMutableArray alloc] init];
+//    }
+//    
+//    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:self.cell inSection:1];
+//    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+//}
+
+
+//// Override to support conditional editing of the table view.
+//- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+//    // Return NO if you do not want the specified item to be editable.
+//    return YES;
+//}
+
+- (IBAction)glucoReview:(id)sender {
+    
+    if ([self.GlucoData.text intValue] >= 200) {
+        self.GlucoData.textColor =[UIColor redColor];
+    }else{
+        if ([self.GlucoData.text intValue] <=75) {
+            self.GlucoData.textColor =[UIColor yellowColor];
+        }else{
+           
+                self.GlucoData.textColor =[UIColor greenColor];
+            
+        }
+    }
+}
+
+
+
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+
+
+
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+
+
+
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+@end
--- TableView/Notes.m
+++ TableView/Notes.m
@@ -1,33 +1,27 @@
 //
-//  TableViewController.m
+//  Notes.m
 //  TableView
 //
-//  Created by Jessica Oliveira on 26/03/15.
+//  Created by Davi Rodrigues on 27/03/15.
 //  Copyright (c) 2015 Jessica Oliveira. All rights reserved.
 //
 
-#import "TableViewController.h"
-#import "ViewController.h"
+#import "Notes.h"
 
-@interface TableViewController ()
-@property (weak, nonatomic) IBOutlet UITextField *GlucoData;
-@property NSMutableArray *objects;
-@property NSInteger cell;
+@interface Notes ()
+
 @end
 
-@implementation TableViewController
-
-
+@implementation Notes
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.cell = 1;
-//     Uncomment the following line to preserve selection between presentations.
-//     self.clearsSelectionOnViewWillAppear = NO;
-//    
-//     Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//     self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,57 +32,36 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-
+#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 2;
+    return 0;
 }
-
-
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
     // Return the number of rows in the section.
-   
-    if(section == 0)
-        self.cell = 1;
-    else
-        self.cell+=3;
-    
-    return self.cell;
+    return 0;
 }
 
-//- (IBAction)moreSymptom:(id)sender {
-//    if (!self.objects) {
-//        self.objects = [[NSMutableArray alloc] init];
-//    }
-//    
-//    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:self.cell inSection:1];
-//    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-//}
-
-
-//// Override to support conditional editing of the table view.
-//- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-//    // Return NO if you do not want the specified item to be editable.
-//    return YES;
-//}
-
-- (IBAction)glucoReview:(id)sender {
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
-    if ([self.GlucoData.text intValue] >= 200) {
-        self.GlucoData.textColor =[UIColor redColor];
-    }else{
-        if ([self.GlucoData.text intValue] <=75) {
-            self.GlucoData.textColor =[UIColor yellowColor];
-        }else{
-           
-                self.GlucoData.textColor =[UIColor greenColor];
-            
-        }
-    }
+    // Configure the cell...
+    
+    return cell;
 }
+*/
 
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
 
-
+/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
@@ -98,23 +71,23 @@
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
+*/
 
-
-
+/*
 // Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
 }
+*/
 
-
-
+/*
 // Override to support conditional rearranging of the table view.
 - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the item to be re-orderable.
     return YES;
 }
+*/
 
-
-
+/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -122,6 +95,6 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-
+*/
 
 @end
