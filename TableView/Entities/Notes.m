f0bef6c07fa46942e3e2bbf0b5ddb4c280ed2103
Arthur Brum <Arthur@MacBook-Pro-de-Arthur.local>
2015-04-02 11:27:56 -0300

    CoreData Classes Added

---
+++ TableView/Entities/Notes.m
@@ -0,0 +1,16 @@
+//
+//  Notes.m
+//  TableView
+//
+//  Created by Arthur Jacunas de Brum on 02/04/15.
+//  Copyright (c) 2015 Jessica Oliveira. All rights reserved.
+//
+
+#import "Notes.h"
+
+
+@implementation Notes
+
+@dynamic note;
+
+@end
